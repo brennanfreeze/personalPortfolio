dbde0eba449a007ba86f265857ab68d8b7c33603
Brennan Freeze <freezebrennan@gmail.com>
2023-06-26 15:21:37 -0700

    Update firebase-config.ts

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA94oJQNcMgL0RoA0j0yCt4FeLOpHKp1Kw',
-  authDomain: 'portfolio-cf690.firebaseapp.com',
-  projectId: 'portfolio-cf690',
-  storageBucket: 'portfolio-cf690.appspot.com',
-  messagingSenderId: '945503066403',
-  appId: '1:945503066403:web:a8f0ca34d753aca1f91496',
-  measurementId: 'G-H767PL819J',
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  projectId: 'projectName',
+  storageBucket: 'storageBucket',
+  messagingSenderId: 'messengerSenderID',
+  appId: 'appId',
+  measurementId: 'masurementId',
 };
 
 export const app = initializeApp(firebaseConfig);
